b7b9163728ddc0afdc77d5d93b78b622bc625efd
Halil Gedik <halilgedik4234@gmail.com>
2023-10-11 09:46:43 +0300

    Cart Controller oluşturuldu, Sepete Ekle butonları oluşturuldu

---
+++ ShopApp.WebUI/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
